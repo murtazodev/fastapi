29dbd31f515cbe21be820a7873e60e9335e2c054
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-16 19:29:37 +0900

    posts crud

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from pprint import pprint
 
 app = FastAPI()
 
+
 class Post(BaseModel):
     title: str
     content: str
     rating: Optional[int] = None
+
 
 my_posts = [
     {
@@ -20,23 +22,43 @@ my_posts = [
         "rating": 5
     },
     {
-        id: 2,
+        "id": 2,
         "title": "Hello World 2",
         "content": "This is my second post",
         "rating": 4
     }
 ]
 
+
+# get operation
+# GET /hello-world
 @app.get("/hello-world")
 async def root():
     return {"message": "Hello Guys!!!"}
 
 
+# get all posts
+# GET /posts
 @app.get("/posts")
 def get_posts():
     return my_posts
 
 
+# get an individual post by id 
+# GET /posts/{id}
+@app.get("/posts/{id}")
+def get_post(id: int):
+    for i in my_posts:
+        if i["id"] == id:
+            return {
+                "message": "Post found",  
+                "post": i
+            }
+    return {"message": "Post not found"}
+
+
+# create a new post
+# POST /posts
 @app.post("/posts")
 def create_post(post: Post):
     converted_post = post.model_dump()
@@ -46,6 +68,3 @@ def create_post(post: Post):
         "message": "Post created successfully",
         "that_post" : converted_post
     }
-
-pprint(my_posts)
-print(len(my_posts))
