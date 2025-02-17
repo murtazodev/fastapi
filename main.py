e2194bc0a4dc139066aa06d4cbc88235dbc6403a
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-17 12:41:54 +0900

    recipe create with recipe ingredient, recipe ingredient model in forms and in create template

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from typing import Optional
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 from random import randrange
-from pprint import pprint
 
 app = FastAPI()
 
@@ -41,25 +39,31 @@ async def root():
 # GET /posts
 @app.get("/posts")
 def get_posts():
-    return my_posts
+    return {
+        "message": "List of Posts",
+        "posts": my_posts
+    }
 
 
 # get an individual post by id 
 # GET /posts/{id}
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
     for i in my_posts:
         if i["id"] == id:
             return {
-                "message": "Post found",  
+                "status_code": status.HTTP_200_OK,  
                 "post": i
             }
-    return {"message": "Post not found"}
+    return {
+            "message": "Post not found",
+            "status_code": status.HTTP_404_NOT_FOUND
+        }
 
 
 # create a new post
 # POST /posts
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     converted_post = post.model_dump()
     converted_post["id"] = randrange(0, 1000000)
@@ -68,3 +72,24 @@ def create_post(post: Post):
         "message": "Post created successfully",
         "that_post" : converted_post
     }
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    for i in my_posts:
+        if i["id"] == id:
+            my_posts.remove(i)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+print(len(my_posts))
+
+class Demo:
+    def __init__(self, x):
+        self.x = x
+    def __call__(self, x):
+        return x + 1
+    
+add_one = Demo(9)
+print(add_one(9))
+
+    
